746704fde59b2b931388bf1a1f6fce133b844490
Danielapariona <daniela.parionac@gmail.com>
2020-09-29 02:12:20 -0500

    Auth layout

--- src/app/modules/auth/login/login.component.ts
+++ src/app/modules/auth/login/login.component.ts
@@ -1,6 +1,11 @@
 import { Component } from "@angular/core";
+import { Page } from "@nativescript/core";
 
 @Component({
     templateUrl: "./login.component.html"
 })
-export class LoginComponent { }
+export class LoginComponent {
+    constructor(private page: Page) {
+        this.page.actionBarHidden = true;
+    }
+}
